54def3cbb82c59bbd2aeaab3eec4ce63235069ba
AryaSonaGinting <aryasgin27@gmail.com>
2025-10-15 22:36:34 +0700

    Add files via upload

---
+++ uts pemweb/script.js
@@ -0,0 +1,7 @@
+document.getElementById("editBtn").addEventListener("click", function() {
+  alert("What u wanna bre? 😏🎶");
+});
+
+console.log("Welcome to halaman Reggae Vibes - Pengenalan HTML 🌞");
+
+console.log("Welcome to halaman CSS — Reggae Vibes 🌴");
